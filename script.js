7303e2b47e86244dda7f7980a91f45ffdee699cb
Bruno de almeida <brunodbck7@gmail.com>
2023-06-12 11:30:39 -0300

    adjusting the code

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function toggleMode() {
   html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
+
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./imagens/avatar-light.png")
   } else {
